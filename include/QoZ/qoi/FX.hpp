aefecd6eec9604fc7502e772e962e95fbc85305a
Jliu-1 <jliu217@central.uh.edu>
2024-09-25 23:19:54 -0500

    singularities

--- include/QoZ/qoi/FX.hpp
+++ include/QoZ/qoi/FX.hpp
@@ -323,6 +323,7 @@ namespace QoZ {
             if (is_a<Pow>(expr)) {
                 auto pow_expr = rcp_static_cast<const Pow>(expr.get_basic());
                 auto exponent = pow_expr->get_exp();
+                std::cout<<*exponent<<std::endl;
 
                 if (is_a<const RealDouble>(*exponent) or is_a<const Integer>(*exponent)) {
                     double exp_val = SymEngine::rcp_static_cast<const SymEngine::RealDouble>(exponent)->as_double();
