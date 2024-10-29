a64aeebe8df167d4f9d8b2762c9be521d7687854
Jliu-1 <jliu217@central.uh.edu>
2024-10-29 01:01:20 -0500

    new global eb

--- include/QoZ/compressor/SZQoIInterpolationCompressor.hpp
+++ include/QoZ/compressor/SZQoIInterpolationCompressor.hpp
@@ -254,7 +254,7 @@ namespace QoZ {
             quant_inds = std::vector<int>(num_elements * 2);//eb + data
             ebs = conf.ebs;
             size_t interp_compressed_size = 0;
-            double eb = conf.absErrorBound;
+            double eb = qoi->get_global_eb();
             quantizer_eb.set_global_eb(eb);
 
             if (start_level<=0 or start_level>interpolation_level ){
