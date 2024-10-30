e972aef6ce7d1435e1aa4e4e033a4e08b85f0c21
Jliu-1 <jliu217@central.uh.edu>
2024-10-30 15:39:20 -0500

    new fixrate

--- include/QoZ/api/impl/SZInterp.hpp
+++ include/QoZ/api/impl/SZInterp.hpp
@@ -212,7 +212,7 @@ void QoI_tuning(QoZ::Config &conf, T *data){
         else{
             std::vector<size_t> quantiles;
             //std::array<double,4> fixrate = {1.0,1.05,1.10,1.15};//or{1.0,1.1,1.2,1.3}
-            std::array<double,5> fixrate = {1.07,1.05,1.03,1.01,1.00};
+            std::array<double,5> fixrate = {1.06,1.04,1.02,1.00,1.00};
             double quantile_split=0.1;
             for(auto i:{1.0,0.5,0.25,0.1,0.05})
                 quantiles.push_back((size_t)(i*k));
