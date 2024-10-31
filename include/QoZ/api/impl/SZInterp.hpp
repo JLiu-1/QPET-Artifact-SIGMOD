f677d18afa8edf9a9aed8de94cbd85f002951659
Jliu-1 <jliu217@central.uh.edu>
2024-10-31 18:28:13 -0500

    more quantiles

--- include/QoZ/api/impl/SZInterp.hpp
+++ include/QoZ/api/impl/SZInterp.hpp
@@ -306,7 +306,7 @@ void QoI_tuning(QoZ::Config &conf, T *data){
             //std::array<double,4> fixrate = {1.0,1.05,1.10,1.15};//or{1.0,1.1,1.2,1.3}
             
             double quantile_split=0.1;
-            for(auto i:{1.0,0.5,0.25,0.10,0.05,0.025,0.01})
+            for(auto i:{2.5,1.0,0.5,0.25,0.10,0.05,0.025,0.01})
                 quantiles.push_back((size_t)(i*k));
             int quantile_num = quantiles.size();
 
@@ -411,7 +411,7 @@ void QoI_tuning(QoZ::Config &conf, T *data){
 
 
             else{
-                std::array<double,5> fixrate = {1.06,1.04,1.02,1.00,1.00};
+                std::array<double,8> fixrate = {1.08,1.06,1.04,1.02,1.00,1.00,1.00,1.00};
 
                 size_t sampling_num, sampling_block;
                 std::vector<size_t> sample_dims(N);
@@ -451,7 +451,8 @@ void QoI_tuning(QoZ::Config &conf, T *data){
                 int idx = 0;
                 for(auto quantile:quantiles)
                 {   
-                    std::nth_element(ebs.begin(),ebs.begin()+quantile, ebs.begin()+last_quantile);
+                    if(idx!=0)
+                        std::nth_element(ebs.begin(),ebs.begin()+quantile, ebs.begin()+last_quantile);
 
                     
                     testConf.absErrorBound = ebs[quantile];
@@ -469,6 +470,9 @@ void QoI_tuning(QoZ::Config &conf, T *data){
                         best_ratio = cur_ratio*fr;
                         best_abs_eb = testConf.absErrorBound;
                         best_quantile = quantile;
+                    }
+                    else{
+                        break;
                     }
                     last_quantile = quantile+1;
 
