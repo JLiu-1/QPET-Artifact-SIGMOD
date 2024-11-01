2a6d2739f04c3cf40124f01f45a9285a22f94882
Jliu-1 <jliu217@central.uh.edu>
2024-11-01 02:44:08 -0500

    test

--- include/QoZ/compressor/SZInterpolationCompressor.hpp
+++ include/QoZ/compressor/SZInterpolationCompressor.hpp
@@ -704,12 +704,14 @@ namespace QoZ {
                 int fd=conf.frozen_dim;
                 if(fd>=0)
                     anchor_strides[fd]=1;
-                
+                std::cout<<maxStep<<" "<<conf.dims[0]<<std::endl;
 
                 
                 for (size_t x=anchor_strides[0]*(tuning==1);x<conf.dims[0];x+=anchor_strides[0]){
                     for (size_t y=anchor_strides[1]*(tuning==1);y<conf.dims[1];y+=anchor_strides[1]){
                         for(size_t z=anchor_strides[2]*(tuning==1);z<conf.dims[2];z+=anchor_strides[2]){
+                            if(tuning == 1)
+                                std::cout<<x<<" "<<y<<" "<<z<<std::endl;
                             quantizer.insert_unpred(*(data+x*dimension_offsets[0]+y*dimension_offsets[1]+z) );
                             quant_inds.push_back(0);
                         }           
