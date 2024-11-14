abbf1a207cfde4d75386b1c5540cfd322c1cd17a
Jliu-1 <jliu217@central.uh.edu>
2024-11-13 22:11:10 -0600

    global correction

--- include/QoZ/api/impl/SZInterp.hpp
+++ include/QoZ/api/impl/SZInterp.hpp
@@ -843,7 +843,7 @@ void QoI_tuning(QoZ::Config &conf, T *data){
                 }
 
 
-                if (conf.qoiRegionMode !=1){
+                if (1){
                 //test full-global mode
                    
                     testConf.absErrorBound = best_abs_eb;
@@ -988,12 +988,12 @@ void QoI_tuning(QoZ::Config &conf, T *data){
 
                 double smaller_ebs_ratio = (double)(count)/(double)(conf.num);
 
-                if( conf.qoiRegionMode !=1 and (smaller_ebs_ratio <= 1.0/1024.0 or min_abs_eb >= 0.95 * best_abs_eb ) ){//may fix
+                if( 1 and (smaller_ebs_ratio <= 1.0/1024.0 or min_abs_eb >= 0.95 * best_abs_eb ) ){//may fix
                     conf.use_global_eb = true;
                     //conf.qoiPtr = qoi;
                 }
 
-                else if (conf.qoiRegionMode !=1){//untested
+                else if (1){//untested
                 //test full-global mode
                    
                     testConf.absErrorBound = best_abs_eb;
