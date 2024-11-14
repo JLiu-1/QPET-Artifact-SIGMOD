b1589f902b612d1af7d73faa5b030b5a76396d39
Jliu-1 <jliu217@central.uh.edu>
2024-11-13 21:38:43 -0600

    global correction

--- include/QoZ/api/impl/SZInterp.hpp
+++ include/QoZ/api/impl/SZInterp.hpp
@@ -649,10 +649,11 @@ void QoI_tuning(QoZ::Config &conf, T *data){
                 rate = estimate_rate_Hoeffdin(num_elements,num_blocks,q, conf.error_std_rate);
             else
                 rate = estimate_rate_Bernstein(num_elements,num_blocks,q, conf.error_std_rate);
+            std::cout<<num_elements<<" "<<num_blocks<<" "<<conf.error_std_rate<<" "<<rate<<std::endl;
             
             rate = std::max(1.0,rate);//only effective for average. general: 1.0/sumai
         }
-        std::cout<<"Point wise QoI eb rate: " << rate << std::endl;
+        std::cout<<"Pointwise QoI eb rate: " << rate << std::endl;
         conf.qoiEB *= rate;
     }
 
@@ -839,7 +840,7 @@ void QoI_tuning(QoZ::Config &conf, T *data){
                 }
 
 
-                if (conf.qoiRegionMode == 0 or (conf.qoiRegionMode == 1 and conf.qoiRegionSize >= 3)){
+                if (conf.qoiRegionMode !=1){
                 //test full-global mode
                    
                     testConf.absErrorBound = best_abs_eb;
@@ -984,12 +985,12 @@ void QoI_tuning(QoZ::Config &conf, T *data){
 
                 double smaller_ebs_ratio = (double)(count)/(double)(conf.num);
 
-                if( (conf.qoiRegionMode == 0 or (conf.qoiRegionMode == 1 and conf.qoiRegionSize >= 3)) and (smaller_ebs_ratio <= 1.0/1024.0 or min_abs_eb >= 0.95 * best_abs_eb ) ){//may fix
+                if( conf.qoiRegionMode !=1 and (smaller_ebs_ratio <= 1.0/1024.0 or min_abs_eb >= 0.95 * best_abs_eb ) ){//may fix
                     conf.use_global_eb = true;
                     //conf.qoiPtr = qoi;
                 }
 
-                else if (conf.qoiRegionMode == 0 or (conf.qoiRegionMode == 1 and conf.qoiRegionSize >= 3)){//untested
+                else if (conf.qoiRegionMode !=1){//untested
                 //test full-global mode
                    
                     testConf.absErrorBound = best_abs_eb;
