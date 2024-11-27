51b6b02a26b633a044f9e142ad5a71029c356e77
Jliu-1 <jliu217@central.uh.edu>
2024-11-27 17:22:27 -0600

    revise

--- include/QoZ/qoi/SymEngine.hpp
+++ include/QoZ/qoi/SymEngine.hpp
@@ -41,7 +41,7 @@ using SymEngine::is_a;
 using SymEngine::FiniteSet;
 
 //template<class T>
-auto convert_expression_to_function(const Basic &expr, const RCP<const Symbol> &x) {
+std::function<double(double)> convert_expression_to_function(const Basic &expr, const RCP<const Symbol> &x) {
         //std::cout<<SymEngine::type_code_name(expr.get_type_code())<<std::endl;
         // x
         if (is_a<const SymEngine::Symbol>(expr)) {
