af0a1b4a58861a604a0a117ff9fd3634fe91b0ab
Jliu-1 <jliu217@central.uh.edu>
2025-01-16 21:19:50 -0600

    composite

--- include/QoZ/qoi/RegionalAverage.hpp
+++ include/QoZ/qoi/RegionalAverage.hpp
@@ -160,7 +160,7 @@ namespace QoZ {
 
         } 
 
-        std::string get_expression() const{
+        std::string get_expression(const std::string var="x") const{
             return "Regional average of x";
         }
 
--- include/QoZ/qoi/RegionalAverageOfSquare.hpp
+++ include/QoZ/qoi/RegionalAverageOfSquare.hpp
@@ -223,7 +223,7 @@ namespace QoZ {
 
         } 
 
-        std::string get_expression() const{
+        std::string get_expression(const std::string var="x") const{
             return "Regional average of x^2";
         }
 
