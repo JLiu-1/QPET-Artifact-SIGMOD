6518ab6044ea76e805817f06986ba985033ae9ae
Jliu-1 <jliu217@central.uh.edu>
2025-01-18 04:02:05 -0600

    new fx

--- include/QoZ/qoi/FX_New.hpp
+++ include/QoZ/qoi/FX_New.hpp
@@ -86,7 +86,7 @@ namespace QoZ {
            //Expression y = Symbol("y");
 
             //auto eb_expression = Expression(Mul( Add( Pow( (Add(Pow(df,2),Mul(2*tolerance,Abs(ddf)))),0.5),neg(Abs(df)) ), Pow(Abs(ddf),-1) ));
-            auto eb_expression = Symbol("x")
+            auto eb_expression = Symbol("x");
             std::cout<<eb_expression <<std::endl;
             //eb_expression = eb_expression.simplify();
             //std::cout<<eb_expression <<std::endl;
@@ -212,7 +212,7 @@ namespace QoZ {
             ddf = df.diff(x);
 
             //auto eb_expression = Expression(Mul( Add( Pow( (Add(Pow(df,2),Mul(2*tolerance,Abs(ddf)))),0.5),neg(Abs(df)) ), Pow(Abs(ddf),-1) ));
-            auto eb_expression = Symbol("x")
+            auto eb_expression = Symbol("x");
 
             std::cout<<eb_expression <<std::endl;
             //eb_expression = eb_expression.simplify();
