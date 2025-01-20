0f38929249de53e6f781d279c594044e616e85bb
Jliu-1 <jliu217@central.uh.edu>
2025-01-20 16:31:14 -0600

    test

--- include/QoZ/api/impl/SZInterp.hpp
+++ include/QoZ/api/impl/SZInterp.hpp
@@ -892,17 +892,29 @@ void QoI_tuning(QoZ::Config &conf, T *data){
             rate = std::max(1.0,rate);//only effective for average. general: 1.0/sumai
 
             if(conf.qoi == 11 or (conf.qoi == 14 and conf.qoi_string == "x")){
-                if(conf.qoiRegionSize <=32){//it is a random number. to Fix
+                if(conf.qoiRegionSize <=8){//it is a random number. to Fix
                     if (conf.QoZ == 0)
                         rate = std::min(2.0,rate);
                     else
                         rate = std::min(4.0,rate);
                 }
-                else{
+                else if(conf.qoiRegionSize <=16){//it is a random number. to Fix
                     if (conf.QoZ == 0)
-                        rate = std::min(20.0,rate);
+                        rate = std::min(4.0,rate);
                     else
-                        rate = std::min(40.0,rate);
+                        rate = std::min(8.0,rate);
+                }
+                else if(conf.qoiRegionSize <=32){//it is a random number. to Fix
+                    if (conf.QoZ == 0)
+                        rate = std::min(8.0,rate);
+                    else
+                        rate = std::min(16.0,rate);
+                }
+                else {
+                    if (conf.QoZ == 0)
+                        rate = std::min(16.0,rate);
+                    else
+                        rate = std::min(32.0,rate);
                 }
                 //else if 
             }
