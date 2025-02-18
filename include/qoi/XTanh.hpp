77ac0dd1c328d3ad7578bd0b978cca013fa8954a
Jliu-1 <jliu217@central.uh.edu>
2025-02-17 21:29:35 -0600

    bugfix

--- include/qoi/XTanh.hpp
+++ include/qoi/XTanh.hpp
@@ -110,13 +110,15 @@ namespace QoZ {
             double t = std::tanh(data);
             double a = 1-t*t;
             double b = fabs(2*a*t);
-            T eb;
-            if(!std::isnan(a) and !std::isnan(b) and !std::isinf(a) and !std::isinf(b)and b >= 1e-10 )
+            T eb = 0;
+            if(!std::isnan(a) and !std::isnan(b) and !std::isinf(a) and !std::isinf(b) and b >=1e-10)
                 eb = (sqrt(a*a+2*b*tolerance)-a)/b;
-            else if (!std::isnan(a) and !std::isinf(a) and a!=0 )
-                eb = tolerance/a;
-            else 
-                eb = global_eb;
+            if(eb==0){
+                if (!std::isnan(a) and !std::isinf(a) and a!=0 )
+                    eb = tolerance/a;
+                else 
+                    eb = global_eb;
+            }
            
             return std::min(eb, global_eb);
         }
